2778c4ed10f382368494c12ccd06cddb2d7c4706
rotrocol <56409559+rotrocol@users.noreply.github.com>
2020-11-08 17:46:41 +0200

    - ml tests added - ml tokenizer

--- src/server/core/ml/machinelearning.py
+++ src/server/core/ml/machinelearning.py
@@ -1,12 +1,15 @@
 from .. import exceptions as core_exceptions
 from .. import interfaces as core_interfaces
-
+from . import tokenizer
 
 class MachineLearning(core_interfaces.AnalyzerStrategy):
     def __init__(self):
         pass
 
     def analyze(self, preprocessed) -> [int]:
+
+        tokens = tokenizer.tokenize(preprocessed)
+
         return []
 
 
--- src/tests/test_ml.py
+++ src/tests/test_ml.py
@@ -7,7 +7,16 @@ def machine_learning():
     return ml.MachineLearning()
 
 def test_1(machine_learning):
-    assert machine_learning.analyze("Fructuti gura mentei") == []
+    assert machine_learning.analyze("") == []
 
 def test_2(machine_learning):
-    assert machine_learning.analyze("Hey pastarnacule") == []
+    assert machine_learning.analyze("Hello friend") == []
+
+def test_3(machine_learning):
+    assert machine_learning.analyze("Have a nice day") == []
+
+def test_4(machine_learning):
+    assert machine_learning.analyze("You are stupid") == [7, 8, 9, 10, 11, 12, 13]
+
+def test_5(machine_learning):
+    assert machine_learning.analyze("You are such an idiot") == [16, 17, 18, 19, 20]
