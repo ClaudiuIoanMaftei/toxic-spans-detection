dfe93d4e37b56ab8fbe102524a4fed717ffa2181
dinosaph <andra.oica@yahoo.com>
2020-11-15 21:11:35 +0200

    DL handler changes

--- src/server/core/dl/cnn/handlers/convolutional_handler.py
+++ src/server/core/dl/cnn/handlers/convolutional_handler.py
@@ -1,10 +1,16 @@
-class ConvolutionalLayer:
+from src.server.core.interfaces import Handler
+
+
+class ConvolutionalLayer(Handler):
     """
     The convolutional neural network is the main layer of the DL component. This will contain the kernel matrices
     that will be executed. This layer must also be trained before usage.
     """
 
     def __init__(self):
+        pass
+
+    def execute(self, args) -> None:
         pass
 
     def train(self, **kwargs):
--- src/server/core/dl/cnn/handlers/fnn_handler.py
+++ src/server/core/dl/cnn/handlers/fnn_handler.py
@@ -1,9 +1,15 @@
-class FeedForwardNetworkLayer:
+from src.server.core.interfaces import Handler
+
+
+class FeedForwardNetworkLayer(Handler):
     """
     The FNN is essentially a `:class:ConvolutionalLayer` that has only a 1x1 kernel size. This is used for fine tuning
     the weights and should be one of the last stages of the execution chain.
     """
     def __init__(self):
+        pass
+
+    def execute(self, args) -> None:
         pass
 
     def train(self, **kwargs):
