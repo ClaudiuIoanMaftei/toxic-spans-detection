26e46dceae87af5ee297e1eb65cc24094b1b3b1b
dinosaph <andra.oica@yahoo.com>
2020-12-21 23:55:54 +0200

    DL init train and vocab call separation

--- src/server/core/dl/__init__.py
+++ src/server/core/dl/__init__.py
@@ -1,7 +1,7 @@
+import sys
 from src.server.core import AnalyzerStrategy
 from src.server.core.dl.cnn.executors.default_execution import DefaultExecution
 from src.server.core.dl.parsing.vocabulary import Vocabulary
-
 
 class DeepLearning(AnalyzerStrategy):
     _execution_strategy = None
@@ -9,28 +9,41 @@ class DeepLearning(AnalyzerStrategy):
 
     def __init__(self):
         self._execution_strategy = DefaultExecution()
-        self.dataset_vocabulary, labels, word_size, sentence_size = Vocabulary.from_csv(
-            "../../../../datasets/tsd_train.csv")
-        self._train({
-            "word_size": word_size,
-            "sent_size": sentence_size,
-            "input": self.dataset_vocabulary,
-            "labels": labels
-        })
+        self.dataset_vocabulary = []
+        self.labels = []
+        self.word_size = 0
+        self.sentence_size = 0
 
     def analyze(self, preprocessed) -> [int]:
         return None
 
+    def _import_vocabulary(self, train_file = "datasets/tsd_train.csv" ):
+        """
+        Imports Vocabulary from external file
+        :param train_file: required training data in csv file
+        """
+        self.dataset_vocabulary, self.labels, self.word_size, self.sentence_size = Vocabulary.from_csv(train_file)
+
     def _train(self, kwargs):
         """
-        Trains the CNN with the given input files
-        :param input_directory: directory with one or more CSV files used as input data
+        Trains the CNN with the given input data
         :return: None
         """
         self.strategy = DefaultExecution()
         self.strategy.init(**kwargs)
 
+    def execute_training(self):
+        self._import_vocabulary()
+        self._train({
+            "word_size": self.word_size,
+            "sent_size": self.sentence_size,
+            "input": self.dataset_vocabulary,
+            "labels": self.labels
+        })
+
 
 if __name__ == "__main__":
     print("Deep Learning")
     dl = DeepLearning()
+    if sys.argv[1] == "train":
+        dl.execute_training()
