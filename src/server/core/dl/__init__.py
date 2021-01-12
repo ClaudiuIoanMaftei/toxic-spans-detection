d9f42ecca7d06b521469180f5c1597bd0ac07df7
dinosaph <andra.oica@yahoo.com>
2021-01-12 20:06:31 +0200

    Dl init changes - serialization

--- src/server/core/dl/__init__.py
+++ src/server/core/dl/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from src.server.core import AnalyzerStrategy
 from src.server.core.dl.cnn.executors.default_execution import DefaultExecution
 from src.server.core.dl.parsing.vocabulary import Vocabulary
@@ -9,17 +10,21 @@ class DeepLearning(AnalyzerStrategy):
 
     def __init__(self):
         self._execution_strategy = DefaultExecution()
-        self.dataset_vocabulary, labels, word_size, sentence_size = Vocabulary.from_csv(
-            "../../../../datasets/tsd_train.csv")
-        self._train({
-            "word_size": word_size,
-            "sent_size": sentence_size,
-            "input": self.dataset_vocabulary,
-            "labels": labels
-        })
+        self.dataset_vocabulary = []
+        self.labels = []
+        self.word_size = 0
+        self.sentence_size = 0
+        self.model = 0
 
     def analyze(self, preprocessed) -> [int]:
         return None
+
+    def _import_vocabulary(self, training_data_path):
+        """
+        Imports Vocabulary from external files
+        :param train_file: required training data in csv files
+        """
+        self.dataset_vocabulary, self.labels, self.word_size, self.sentence_size = Vocabulary.from_csv(training_data_path)
 
     def _train(self, kwargs):
         """
@@ -28,9 +33,25 @@ class DeepLearning(AnalyzerStrategy):
         :return: None
         """
         self.strategy = DefaultExecution()
-        self.strategy.init(**kwargs)
+        self.model = self.strategy.init(**kwargs)
+        if self.model is not None:
+            self.model.save("fashion_model_dropout.h5py")
+
+    def execute_training(self, training_data_path = "../../../../datasets/tsd_train.csv"):
+        self._import_vocabulary(training_data_path)
+        self._train({
+            "word_size": self.word_size,
+            "sent_size": self.sentence_size,
+            "input": self.dataset_vocabulary,
+            "labels": self.labels
+        })
 
 
 if __name__ == "__main__":
     print("Deep Learning")
     dl = DeepLearning()
+    if sys.argv[1] == "-train":
+        try:
+            dl.execute_training(sys.argv[2])
+        except:
+            dl.execute_training()
